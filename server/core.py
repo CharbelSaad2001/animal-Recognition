be38ca7b3a0e87a099b5d55f83e24186ab43c1b2
Alejandro Rosas <alejandrojrosas2001@gmail.com>
2025-01-02 19:32:06 -0400

    ahre

--- server/core.py
+++ server/core.py
@@ -1,13 +1,23 @@
 import numpy as np
-from PytorchWildlife.models import detection as pw_detection
-from PytorchWildlife.models import classification as pw_classification
+# TODO: Al importar se rompe todo :(
+# from PytorchWildlife.models import detection as pw_detection
+# from PytorchWildlife.models import classification as pw_classification
 
 img = np.random.randn(3, 1280, 1280)
 
 # Detection
-detection_model = pw_detection.MegaDetectorV5() # Model weights are automatically downloaded.
-detection_result = detection_model.single_image_detection(img)
+# detection_model = pw_detection.MegaDetectorV5() # Model weights are automatically downloaded.
+# detection_result = detection_model.single_image_detection(img)
 
 #Classification
-classification_model = pw_classification.AI4GAmazonRainforest() # Model weights are automatically downloaded.
-classification_results = classification_model.single_image_classification(img)
+# classification_model = pw_classification.AI4GAmazonRainforest() # Model weights are automatically downloaded.
+# classification_results = classification_model.single_image_classification(img)
+
+def classify_image(img_array):
+    # PytorchWildlife.models.classification.AI4GAmazonRainforest().single_image_classification(img_array)
+    print("Hello World", img)
+    print("Classifying image...", img_array)
+
+    return {
+        "result": "Hello World",
+    }
