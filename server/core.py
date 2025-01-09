602657382810b51ff1253c14728d2c2edb9b4552
LuisGonzalez <luisgonzalezsalazar03@gmail.com>
2025-01-09 14:38:51 -0400

    feat: prediction model

--- server/core.py
+++ server/core.py
@@ -1,19 +1,43 @@
 import numpy as np
+import tensorflow as tf
+from PIL import Image
+from io import BytesIO
+from fastapi.responses import JSONResponse
+from dictionary import get_animal_info
 
-img = np.random.randn(3, 1280, 1280)
 
-# Detection
-# detection_model = pw_detection.MegaDetectorV5() # Model weights are automatically downloaded.
-# detection_result = detection_model.single_image_detection(img)
+def preprocess_image(image_data):
+    image = Image.open(BytesIO(image_data)).convert('RGB')
+    image = image.resize((180, 180))  # Resize to match model input
+    image_array = np.array(image) / 255.0  # Normalize the image
+    return np.expand_dims(image_array, axis=0)
 
-#Classification
-# classification_model = pw_classification.AI4GAmazonRainforest() # Model weights are automatically downloaded.
-# classification_results = classification_model.single_image_classification(img)
+#TODO: No se si las predicciones empiezane en 0 o en 1
+CLASSES = {
+    0: "armadillo_gigante",
+    1: "capibara",
+    2: "coati_de_nariz_blanca",
+    3: "jaguar",
+    4: "mono_ardilla",
+    5: "ocelote",
+    6: "oso_hormiguero gigante",
+    7: "pecari_de_labios_blancos",
+    8: "puma",
+    9: "tamandua",
+    10: "tapir_amazonico",
+}
 
-def classify_image(img_array):
-    print("Classifying image...", img_array)
+async def classify_image(img_file):
+    print("Classifying image...", img_file)
     
+    model = tf.keras.models.load_model("animal_classifier_model.keras")
+    
+    image_data = await img_file.read()
+    
+    processed_image = preprocess_image(image_data)
+     
+    prediction = model.predict(processed_image)
+    
+    predicted_class = CLASSES[np.argmax(prediction)]
 
-    return {
-        "result": "Hello World",
-    }
+    return JSONResponse(content={"animal": get_animal_info(predicted_class), "prediction": predicted_class, "confidence": float(np.max(prediction))})
--- server/main.py
+++ server/main.py
@@ -22,22 +22,7 @@ def animal(nombre_comun: str):
 
 @app.post("/upload-image/")
 async def upload_image(file: UploadFile = File(...)):
-    print("Hello World")
-    image_data = await file.read()
 
-    image = Image.open(io.BytesIO(image_data))
-    image = image.resize((1280, 1280))
-    img_array = np.array(image)
-
-    # Asegúrate de que la imagen tenga el formato correcto (C, H, W)
-    if img_array.ndim == 2:  # Si es una imagen en escala de grises
-        img_array = np.stack((img_array,) * 3, axis=-1)  # Convertir a RGB
-    img_array = img_array.transpose(2, 0, 1)  # Cambiar a (C, H, W)
-
-    # TODO: Quitar cuando se implemente la clasificación
-    original_image_path = os.path.join(UPLOADS_DIR, f"original_{file.filename}")
-    image.save(original_image_path)
-
-    result = classify_image(img_array)
+    result = await classify_image(file)
 
     return result
