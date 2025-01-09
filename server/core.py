d9c95e5899a510acf81eb53faf585dd74a0dea2d
LuisGonzalez <luisgonzalezsalazar03@gmail.com>
2025-01-09 12:28:46 -0400

    invento mio

--- server/core.py
+++ server/core.py
@@ -14,9 +14,8 @@ img = np.random.randn(3, 1280, 1280)
 # classification_results = classification_model.single_image_classification(img)
 
 def classify_image(img_array):
-    # PytorchWildlife.models.classification.AI4GAmazonRainforest().single_image_classification(img_array)
-    print("Hello World", img)
     print("Classifying image...", img_array)
+    
 
     return {
         "result": "Hello World",
