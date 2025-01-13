cfb869d6b01d9e6c080ad2400ca890f081ded146
Alejandro Rosas <61267550+AlejandroJRosas@users.noreply.github.com>
2025-01-13 13:01:01 -0400

    feat(server): resolve dependency requirements

--- server/main.py
+++ server/main.py
@@ -1,28 +1,16 @@
 from fastapi import FastAPI, File, UploadFile
-import numpy as np
-from PIL import Image
-import io
-import os
 from core import classify_image
-from dictionary import get_animal_info
 
-app = FastAPI(title="Amazon Rainforest Wildlife Detection and Classification API", version="0.1.0")
-
-
-UPLOADS_DIR = "uploads"
-os.makedirs(UPLOADS_DIR, exist_ok=True)
+app = FastAPI(
+    title="Amazon Rainforest Wildlife Detection and Classification API", 
+    version="0.1.0"
+)
 
 @app.get("/hi")
 def read_root():
     return {"Hello": "Worlds"}
 
-@app.get("/animals/{nombre_comun}")
-def animal(nombre_comun: str):
-    return get_animal_info(nombre_comun)
-
-@app.post("/upload-image/")
+@app.post("/upload-image")
 async def upload_image(file: UploadFile = File(...)):
-
     result = await classify_image(file)
-
     return result
